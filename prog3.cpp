1a92b921ced5da302afdc2ec5bacf74bf84d873c
jatin22malik <jatin25csu095@ncuindia.edu>
2026-01-19 21:44:00 +0530

    Add files via upload

---
+++ prog3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+int a=10;
+float b=20;
+char c='a';
+
+std::cout<<"My age is "<<a<<std::endl<<"my friend age is "<<b<<std::endl<<"How are you?? "<<c;
+
+return 0;
+
+}
