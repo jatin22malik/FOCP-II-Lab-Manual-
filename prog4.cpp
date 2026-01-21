740bf47193ee004175ce5b9e5506714d5bf45a35
jatin22malik <jatin25csu095@ncuindia.edu>
2026-01-21 22:54:27 +0530

    Add files via upload

---
+++ prog4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+
+int main()
+{
+    int a;
+    std::string s;
+    std::cout<<"\nEnter values:";
+    std::cin>>a>>s;
+    std::cout<<"\nWhat is your umar "<<a<<std::endl<<"Which subject are you studying? "<<s;
+    
+
+
+    return 0;
+}
